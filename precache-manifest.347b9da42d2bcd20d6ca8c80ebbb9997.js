66cf786b7c28026ea2ab6c875bbc71c35edc87cb
adrielom <adrielom.dev@gmail.com>
2020-09-13 10:39:53 -0300

    Updates

---
+++ precache-manifest.347b9da42d2bcd20d6ca8c80ebbb9997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9762afcdf49812015afdb693efd26f66",
+    "url": "/imgToVectFrontWeb/index.html"
+  },
+  {
+    "revision": "f2c391fa26edc9ee3e8a",
+    "url": "/imgToVectFrontWeb/static/css/main.6b2cd230.chunk.css"
+  },
+  {
+    "revision": "e981d7b883609d38c092",
+    "url": "/imgToVectFrontWeb/static/js/2.cbe63ffc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imgToVectFrontWeb/static/js/2.cbe63ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c391fa26edc9ee3e8a",
+    "url": "/imgToVectFrontWeb/static/js/main.acb128e7.chunk.js"
+  },
+  {
+    "revision": "656f622a485114937269",
+    "url": "/imgToVectFrontWeb/static/js/runtime-main.987d3c45.js"
+  }
+]);
